3e0d7508da305b4109da9c49b6116976ec767a5f
Oksana Grishchenko <oksana.grishchenko@percona.com>
2026-07-16 14:04:19 +0500

    init
    Signed-off-by: Oksana Grishchenko <oksana.grishchenko@percona.com>

---
+++ definition/backupclasses/pg/types.go
@@ -0,0 +1,20 @@
+// Package pg contains the schema-bearing Go types for the
+// "pg" BackupClass. Each struct here is converted to an OpenAPI
+// v3 schema by `provider-sdk generate` and inlined into the generated
+// BackupClass manifest.
+//
+// +k8s:openapi-gen=true
+package pg
+
+// PgBackupConfig describes the configuration accepted by Backup CRs that
+// target this class (spec.config). Add fields the user can set per backup.
+type PgBackupConfig struct{}
+
+// PgRestoreConfig describes the configuration accepted by Restore CRs that
+// target this class (spec.config). Add fields the user can set per restore.
+type PgRestoreConfig struct{}
+
+// PgPITRConfig describes the per-storage PITR custom config exposed to
+// Instance.spec.backup.storages[].pitr.config. Add fields a provider needs
+// to fine-tune its PITR pipeline (oplog span, compression, retention, etc.).
+type PgPITRConfig struct{}
